d6251d21734f52846cd68a8d88afe7457271b103
Angad <thethifrog@gmail.com>
2022-11-02 16:19:03 +0000

    debugging, some organiational adjustment

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # import statements
 import isTouching
+import random
+# code
+# values hardcoded as placeholder
+rect_w, rect_h = 10, 10
+noofshapes = 14
+coords = [[0, 0], [0, 2], [2, 2], [2, 3], [3, 3], [3, 1], [2, 1], [2, 0]]
+shape_w, shape_h = isTouching.finddimensions(coords)
+shapedict = {}
+# Creating shape objects at random coordinates
+for i in range(1, noofshapes + 1):
+    variablename = "s" + str(i)
+    shapedict.update({variablename: isTouching.Shape(random.randint(0, rect_w - shape_w), random.randint(0, rect_h - shape_h), coords)})
+    print(shapedict[variablename].getlines())
