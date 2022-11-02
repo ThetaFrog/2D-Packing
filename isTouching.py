9d8fe00e4c78702ab6e32aa707fd8fc65b211861
Angad <thethifrog@gmail.com>
2022-11-02 19:17:59 +0000

    complete isTouching function

--- isTouching.py
+++ isTouching.py
@@ -1,7 +1,30 @@
-# import statements
-
 # function
+def checktouching(shape_1, shape_2):
+    linesshape_1 = shape_1.getlines()
+    linesshape_2 = shape_2.getlines()
+    touching = False
+    intersecting = False
+    for i in linesshape_1:
+        for j in linesshape_2:
+            if i[1][0] == 0 and j[1][0] == 0: # both lines horizontal
+                if (j[0][0] + j[1][0] >= i[0][0] >= j[0][0]) or (j[0][0] >= i[0][0] >= j[0][0] + j[1][0]) or (i[0][0] + i[1][0] >= j[0][0] >= i[0][0]) or (i[0][0] >= j[0][0] >= i[0][0] + i[1][0]):
+                    touching = True
+                    continue
+            elif  i[1][1] == 0 and j[1][1] == 0: # both lines vertical
+                if (j[0][1] + j[1][1] >= i[0][1] >= j[0][1]) or (j[0][1] >= i[0][1] >= j[0][1] + j[1][1]) or (i[0][1] + i[1][1] >= j[0][1] >= i[0][1]) or (i[0][1] >= j[0][1] >= i[0][1] + i[1][1]):
+                    touching = True
+                    continue
+            elif (((j[0][0] + j[1][0] >= i[0][0] >= j[0][0]) or (j[0][0] >= i[0][0] >= j[0][0] + j[1][0])) and ((i[0][1] + i[1][1] >= j[0][1] >= i[0][1]) or (i[0][1] >= j[0][1] >= i[0][1] + i[1][1]))) or (((i[0][0] + i[1][0] >= j[0][0] >= i[0][0]) or (i[0][0] >= j[0][0] >= i[0][0] + i[1][0])) and ((j[0][1] + j[1][1] >= i[0][1] >= j[0][1]) or (j[0][1] >= i[0][1] >= j[0][1] + j[1][1]))):
+                intersecting = True
+                break
+                outerloopbreak = True
+        if outerloopbreak:
+            break
 
-
-def checktouching
+    if intersecting:
+        return "intersecting"
+    elif touching:
+        return "touching"
+    else:
+        return "free"
 
--- main.py
+++ main.py
@@ -1,41 +1,7 @@
 # import statements
 import isTouching
+import shape
 import random
-
-
-# classes
-class Shape:
-    def __init__(self, x, y, coordsforshape):  # coords forshape formatted as [[x,y],[x,y]]
-        self.x = x
-        self.y = y
-        self.coordsforshape = coordsforshape
-
-    def shiftcoords(self):
-        shiftedcoords = []
-        for i in range(0, len(self.coordsforshape)):
-
-            shiftedcoords.append([self.coordsforshape[i][0] + self.x, self.coordsforshape[i][1] + self.y])
-        return shiftedcoords
-
-    def getlines(self):
-        linesforshape = []
-        shiftedcoords = self.shiftcoords()
-        for i in range(0, len(shiftedcoords)):
-            if i < len(self.coordsforshape) - 1:
-                linesforshape.append([shiftedcoords[i], shiftedcoords[i + 1]])
-            else:
-                linesforshape.append([shiftedcoords[i], shiftedcoords[0]])
-        return linesforshape
-
-
-# functions
-def finddimensions(shape_coords):
-    x_all = []
-    y_all = []
-    for j in shape_coords:
-        x_all.append(j[0])
-        y_all.append(j[1])
-    return max(x_all), max(y_all)
 
 
 # code
@@ -43,10 +9,10 @@ def finddimensions(shape_coords):
 rect_w, rect_h = 10, 10
 noofshapes = 14
 coords = [[0, 0], [0, 2], [2, 2], [2, 3], [3, 3], [3, 1], [2, 1], [2, 0]]
-shape_w, shape_h = isTouching.finddimensions(coords)
+shape_w, shape_h = shape.finddimensions(coords)
 shapedict = {}
 # Creating shape objects at random coordinates
 for i in range(1, noofshapes + 1):
     variablename = "s" + str(i)
-    shapedict.update({variablename: isTouching.Shape(random.randint(0, rect_w - shape_w), random.randint(0, rect_h - shape_h), coords)})
+    shapedict.update({variablename: shape.Shape(random.randint(0, rect_w - shape_w), random.randint(0, rect_h - shape_h), coords)})
     print(shapedict[variablename].getlines())
